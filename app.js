526219b9595df8af571a052f2d61233485df3117
noel-emmanuel <helloitsnoel@gmail.com>
2020-07-26 17:55:39 +1000

    an array of images

--- app.js
+++ app.js
@@ -4,18 +4,26 @@ console.log("yo the js file is linked");
 let request = new XMLHttpRequest();
 
 // opening a new connection using get
-request.open('GET', 'https://api.are.na/v2/blocks/852210/');
+let api_url = 'https://api.are.na/v2/channels/personal-style-gpepcau7jt4';
+request.open('GET', api_url);
 
 // need to access data as soon as it loaded
 request.onload = function() {
   let data = JSON.parse(this.response);
-  let img = data.image.original.url;
-  console.log(img);
-  var a = document.createElement("img");
-  a.height = 500;
-  a.src = img;
-  console.log(a);
-  document.getElementById("photolink").appendChild(a);
+  let arr = data.contents;
+  for (var i = 0; i < 6; i++) {
+    // console.log(arr[i].image.original.url);
+    images = arr[i].image.original.url;
+    // console.log(images); 
+    var a = document.createElement("img");
+    a.height = 500;
+    a.src = images;
+    // console.log(a);
+    document.getElementById("photolink").appendChild(a);
+
+  }
+  // let img = data.image.original.url;
+  // console.log(img);
 }
 
 
