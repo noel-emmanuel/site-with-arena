567ff12e94c11a5015efbf9760bd152bcb0e6b4d
noel-emmanuel <helloitsnoel@gmail.com>
2020-07-26 23:56:20 +1000

    shows string

--- app.js
+++ app.js
@@ -4,26 +4,19 @@ console.log("yo the js file is linked");
 let request = new XMLHttpRequest();
 
 // opening a new connection using get
-let api_url = 'https://api.are.na/v2/channels/personal-style-gpepcau7jt4';
+let api_url = 'https://api.are.na/v2/channels/a-catalogue-of-simple-pleasures';
 request.open('GET', api_url);
 
 // need to access data as soon as it loaded
 request.onload = function() {
   let data = JSON.parse(this.response);
-  let arr = data.contents;
-  for (var i = 0; i < 6; i++) {
-    // console.log(arr[i].image.original.url);
-    images = arr[i].image.original.url;
-    // console.log(images); 
-    var a = document.createElement("img");
-    a.height = 500;
-    a.src = images;
-    // console.log(a);
-    document.getElementById("photolink").appendChild(a);
-
+  let textContent = data.contents;
+  // let textContent = data.contents['1'].content); // just a pointer
+  for (var i = 0; i < textContent.length; i++) {
+    let returnedStrings = textContent[i].content;
+    let accessWebpage = document.getElementById('root');
+    console.log(accessWebpage.innerHTML = returnedStrings);
   }
-  // let img = data.image.original.url;
-  // console.log(img);
 }
 
 
